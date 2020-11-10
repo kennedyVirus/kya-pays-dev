13d2bb78a19e32d2eaf457ba7924478d4b06a8a5
kennedyVirus <44835221+kennedyVirus@users.noreply.github.com>
2020-11-10 23:14:44 +0000

    getting country selected

--- web/assets/js/pay-transaction.js
+++ web/assets/js/pay-transaction.js
@@ -59,24 +59,30 @@ window.onload = function () {
         mounted: function (){
             // $('.selectpicker').selectpicker();
             //
-            // axios.get('/8004064b17546e4380ce83d1be75b50dkfj2015/api/kya/rest/country/get')
-            //     .then((response)=>{
-            //         if(response.data.error===0){
-            //             this.countries=response.data.data
-            //             //
-            //             if(this.countries.length >0){
-            //                 this.countries.forEach((country,i)=>{
-            //                     // var countr = "<option value=\""+ i +"\" data-tokens=\\\"\"+ country.name +\"\\\">"+ country.name +"</option>\n";
-            //                     // var countr = "<option value=\""+ i +"\" data-tokens=\""+ country.name +"\">"+ country.name +"</option>\n";
-            //                     var countr = "<option value=\""+ i +"\">"+ country.name +"</option>\n";
-            //
-            //                     $("#country_student").after(countr);
-            //                 })
-            //             }
-            //             //
-            //
-            //             }
-            //         })
+            axios.get('/8004064b17546e4380ce83d1be75b50dkfj2015/api/kya/rest/country/get')
+                .then((response)=>{
+                    if(response.data.error===0){
+                        this.countries=response.data.data
+                        //
+                        if(this.countries.length >0){
+                            this.countries.forEach((country,i)=>{
+                                // var countr = "<option value=\""+ i +"\" data-tokens=\\\"\"+ country.name +\"\\\">"+ country.name +"</option>\n";
+                                // var countr = "<option value=\""+ i +"\" data-tokens=\""+ country.name +"\">"+ country.name +"</option>\n";
+                                var countr = "<option value=\""+ i +"\">"+ country.name +"</option>\n";
+
+                                $("#country_student").after(countr);
+                            })
+                        }
+                        //
+
+                        }
+                    }).catch((error)=>{})
+
+            $('select#country_student').on('change', function () {
+                let selection=$('select#country_student').val();
+
+                console.log('country selection==>',selection)
+            })
 
 
             //integer value validation
